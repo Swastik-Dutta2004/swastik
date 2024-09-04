30441c505ef1eafffe7ec0b087e39c0b537aa094
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2024-09-04 20:27:56 +0530

    In this program i have calculated the numbers of platypus and taiga form the list by using count .

---
+++ python/2nd .py
@@ -0,0 +1,9 @@
+world_animals = ["Anaconda", "Platypus", "Red panda", "Beaver", "Killer whale", "Platypus", "Camel", "Polar bear", "King penguin", "Snow leopard", "Zebra", "Plains bison"]
+world_biomes = ["Tropical Rainforest", "Temperate Forest", "Taiga", "Marine", "Freshwater", "Desert", "Arctic Tundra", "Antarctic Tundra", "Alpine Tundra", "Tropical Grassland", "Temperate Grassland"]
+count1 = "Platypus"
+count2 = "Taiga"
+count_platypus = world_animals.count( count1)
+count_tiaga = world_biomes.count(count2)
+
+print(f"platypus apperd {count_platypus} times in world_animals")
+print(f"tiaga apperd {count_tiaga} times in world_tiaga")
